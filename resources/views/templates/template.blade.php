091d6016689021e5feff516e47bd933f9273779f
cammygames <multicraftserver@ymail.com>
2016-01-17 22:30:29 +0000

    Whoops

--- resources/views/templates/template.blade.php
+++ resources/views/templates/template.blade.php
@@ -11,9 +11,9 @@
         @include('templates.footer')
     
         <script src="{{ elixir('js/app.js') }}"></script>
-
-        @if($_ENV['APP_ENV'] == 'production')
-            <script>(function (i, s, o, g, r, a, m) {
+        @stack('scripts')
+        
+        <script>(function (i, s, o, g, r, a, m) {
                     i['GoogleAnalyticsObject'] = r;
                     i[r] = i[r] || function () {
                                 (i[r].q = i[r].q || []).push(arguments)
@@ -25,8 +25,5 @@
                 })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
                 ga('create', 'UA-69463242-3', 'auto');
                 ga('send', 'pageview');</script>
-        @endif
-
-        @stack('scripts')
     </body>
 </html>
